aec9fcc8ba54af1489e300d07a84e514d51d69e7
W. Bosma <wowtor@users.sourceforge.net>
2015-08-06 15:27:32 +0200

    deprecated deployment parameter repositoryBranch, in favor of codenames, which is an array and hence supports multiple values

--- src/main/java/net/sf/debianmaven/RepreproDeployMojo.java
+++ src/main/java/net/sf/debianmaven/RepreproDeployMojo.java
@@ -2,8 +2,12 @@ package net.sf.debianmaven;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
 
 /**
  * @goal deploy
@@ -11,10 +15,19 @@ import org.apache.maven.plugin.MojoExecutionException;
  */
 public class RepreproDeployMojo extends AbstractRepreproMojo
 {
+	private static final String DEFAULT_CODENAME = "experimental";
 	/**
+	 * @deprecated
+	 * @see #repositoryCodenames
 	 * @parameter expression="${deb.repository.branch}" default-value="experimental"
 	 */
 	protected String repositoryBranch;
+
+	/**
+	 * @parameter expression="${deb.repository.codenames}"
+	 * @since 1.0.7
+	 */
+	protected String[] codenames;
 
 	/**
 	 * @parameter expression="${deb.deploy.skipMissing}" default-value="false"
@@ -22,11 +35,26 @@ public class RepreproDeployMojo extends AbstractRepreproMojo
 	 */
 	protected boolean skipDeployMissing;
 
+	@SuppressWarnings("unchecked")
 	private void runReprepro() throws IOException, MojoExecutionException
 	{
 		File pkgfile = getPackageFile();
 		if (pkgfile.exists())
-			runProcess(new String[]{"reprepro", "--confdir", repreproConfigurationDir.toString(), "--basedir", repository.toString(), "includedeb", repositoryBranch, pkgfile.toString()}, true);
+		{
+			List<String> codenames = new ArrayList<String>();
+			if (repositoryBranch != null)
+			{
+				codenames.add(repositoryBranch);
+				getLog().warn("parameter 'repositoryBranch' is now deprecated; use 'codenames' instead");
+			}
+			if (this.codenames != null)
+				codenames.addAll(Arrays.asList(this.codenames));
+			if (codenames.isEmpty())
+				codenames.add(DEFAULT_CODENAME);
+
+			for (String codename : codenames)
+				runProcess(new String[]{"reprepro", "--confdir", repreproConfigurationDir.toString(), "--basedir", repository.toString(), "includedeb", codename, pkgfile.toString()}, true);
+		}
 		else
 			getLog().info("Skipping deployment of non-existent package: "+pkgfile);
 	}
